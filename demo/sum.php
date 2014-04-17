5d766e8fc931fee391049fc5ef2f0a258f9f4346
Jason Leung <leungxp@gmail.com>
2014-04-17 17:03:31 +0800

    change some code to test easily

--- demo/sum.php
+++ demo/sum.php
@@ -4,8 +4,31 @@ require('../autoload.php');
 
 use Gregwar\Tex2png\Tex2png;
 
-echo "Generating sum.png...\n";
+session_start();
 
-Tex2png::create('\sum_{i = 0}^{i = n} \frac{i}{2}')
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+	//echo "Generating sum.png...\n";
+
+	Tex2png::create($_REQUEST['tex'])
     ->saveTo('sum.png')
     ->generate();
+
+    $_SESSION['tex'] = $_REQUEST['tex'];
+
+    header('Location: ' . $_SERVER["REQUEST_URI"]);
+    die();
+}
+
+?>
+<img src="sum.png?r=<?php echo rand()  ?>" />
+<hr />
+<form action="" method="post">
+	<? if(isset($_SESSION['tex'])): ?>
+	<textarea name="tex" rows="3" style="width:500px"><?php echo $_SESSION['tex'] ?></textarea>
+	<? else: ?>
+	<textarea name="tex" rows="3" style="width:500px">\sum_{i = 0}^{i = n} \frac{i}{2}</textarea>
+	<? endif; ?>
+	<br>
+	<input type="submit" value="Submit" />
+</form>
